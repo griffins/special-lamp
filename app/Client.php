53c799319231138d959ee4690edb1ac088f4a563
Griffins Oringo <oringogriffins78@gmail.com>
2019-11-01 10:50:21 +0300

    with

--- app/Client.php
+++ app/Client.php
@@ -84,7 +84,7 @@ class Client extends Authenticatable implements MustVerifyEmail
     {
         return ($this->transactions()
             ->where('date', '<=', $date)
-            ->selectRaw("sum( CASE WHEN type = 'withdrawal' THEN 0 - amount ELSE amount END ) as aggregate")->value('aggregate')) ?: 0;
+            ->selectRaw("sum( CASE WHEN type = 'withdraw' THEN 0 - amount ELSE amount END ) as aggregate")->value('aggregate')) ?: 0;
     }
 
     public function tickets()
