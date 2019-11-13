39feb764f03afd03bcffe9633983024c4d0712a8
Griffins Oringo <oringogriffins78@gmail.com>
2019-11-13 16:07:27 +0300

    dos

--- app/Jobs/InvestorComputation.php
+++ app/Jobs/InvestorComputation.php
@@ -35,34 +35,38 @@ class InvestorComputation implements ShouldQueue
         $account->investorTransactions()->whereIn('type', ['profit', 'buy', 'sell'])->delete();
         foreach ($account->transactions()->whereIn('type', ['sell', 'buy'])->whereNotNull('closed_at')->orderBy('closed_at', 'asc')->cursor(0) as $transaction) {
             foreach ($investors as $investor) {
-                if (!$investor->investorTransactions()->where('transaction_id', $transaction->id)->exists()) {
-                    $balance = $investor->depositAt($transaction->opened_at);
-                    $ratio = $investor->depositAt($transaction->opened_at) / 1000;
-                    $amount = ($transaction->profit + $transaction->swap + $transaction->commission) * 0.7;
-                    if ($balance !== 0) {
-                        $profit2 = ((100 - $investor->commission) / 100) * $amount * $ratio;
-                        $profit = ($investor->commission / 100) * $amount * $ratio;
-                        if ($profit != 0) {
-                            $t = new InvestorTransaction([
-                                'transaction_id' => $transaction->id,
-                                'investor_id' => $investor->id,
-                                'amount' => $profit,
-                                'narration' => $transaction->item,
-                                'type' => $transaction->type,
-                                'date' => $transaction->closed_at,
-                            ]);
-                            $t->save();
-                        }
-                        if ($profit2 != 0) {
-                            $t = new InvestorTransaction([
-                                'transaction_id' => $transaction->id,
-                                'investor_id' => Client::query()->first()->id,
-                                'amount' => $profit2,
-                                'narration' => $transaction->item,
-                                'type' => $transaction->type,
-                                'date' => $transaction->closed_at,
-                            ]);
-                            $t->save();
+                if ($investor->investorTransactions()->where('account_id', $account->id)->deposits() > 0) {
+                    if (!$investor->investorTransactions()->where('transaction_id', $transaction->id)->exists()) {
+                        $balance = $investor->depositAt($transaction->opened_at);
+                        $ratio = $investor->depositAt($transaction->opened_at) / 1000;
+                        $amount = ($transaction->profit + $transaction->swap + $transaction->commission) * 0.7;
+                        if ($balance !== 0) {
+                            $profit2 = ((100 - $investor->commission) / 100) * $amount * $ratio;
+                            $profit = ($investor->commission / 100) * $amount * $ratio;
+                            if ($profit != 0) {
+                                $t = new InvestorTransaction([
+                                    'transaction_id' => $transaction->id,
+                                    'investor_id' => $investor->id,
+                                    'account_id' => $account->id,
+                                    'amount' => $profit,
+                                    'narration' => $transaction->item,
+                                    'type' => $transaction->type,
+                                    'date' => $transaction->closed_at,
+                                ]);
+                                $t->save();
+                            }
+                            if ($profit2 != 0) {
+                                $t = new InvestorTransaction([
+                                    'transaction_id' => $transaction->id,
+                                    'investor_id' => Client::query()->first()->id,
+                                    'account_id' => $account->id,
+                                    'amount' => $profit2,
+                                    'narration' => $transaction->item,
+                                    'type' => $transaction->type,
+                                    'date' => $transaction->closed_at,
+                                ]);
+                                $t->save();
+                            }
                         }
                     }
                 }
